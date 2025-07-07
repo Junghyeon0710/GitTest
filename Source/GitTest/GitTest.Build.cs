7bce203428acda85075f771eacecb4a3ea4708c3
안중현 <133496610+Junghyeon0710@users.noreply.github.com>
2025-07-07 15:02:54 +0900

    Feat tttt (#5)
    * asd
    
    * zxczcx

--- Source/GitTest/GitTest.Build.cs
+++ Source/GitTest/GitTest.Build.cs
@@ -13,9 +13,9 @@ public class GitTest : ModuleRules
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });asdasd
 		
-		// Uncomment if you are using online features
+		// Uncomment if you are using online featuresasdsadasd
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
