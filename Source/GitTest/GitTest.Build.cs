69f5b3645882e37606b24c214c8d0eecf8fe6f51
Junghyeon0710 <sie08357@naver.com>
2025-07-07 15:02:10 +0900

    zxczcx

--- Source/GitTest/GitTest.Build.cs
+++ Source/GitTest/GitTest.Build.cs
@@ -13,7 +13,7 @@ public class GitTest : ModuleRules
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });asdasd
 		
 		// Uncomment if you are using online featuresasdsadasd
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
