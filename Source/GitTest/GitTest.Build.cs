cabca097dcb3adac4f65e117362595d7e7977357
Junghyeon0710 <sie08357@naver.com>
2025-07-07 15:16:58 +0900

    머지용

--- Source/GitTest/GitTest.Build.cs
+++ Source/GitTest/GitTest.Build.cs
@@ -19,5 +19,6 @@ public class GitTest : ModuleRules
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		//머지할려고
 	}
 }
