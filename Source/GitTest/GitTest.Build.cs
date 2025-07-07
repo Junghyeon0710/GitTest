5395a5230551c91c9cd64d5ed6d914a7a04ac37a
Junghyeon0710 <sie08357@naver.com>
2025-07-07 15:02:04 +0900

    asd

--- Source/GitTest/GitTest.Build.cs
+++ Source/GitTest/GitTest.Build.cs
@@ -15,7 +15,7 @@ public class GitTest : ModuleRules
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
-		// Uncomment if you are using online features
+		// Uncomment if you are using online featuresasdsadasd
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
