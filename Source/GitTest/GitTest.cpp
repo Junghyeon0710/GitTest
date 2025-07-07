3ad194c49b3ffa17a825fcbe46570b28ae573ba4
Junghyeon0710 <sie08357@naver.com>
2025-07-07 15:01:35 +0900

    Init

--- Source/GitTest/GitTest.cpp
+++ Source/GitTest/GitTest.cpp
@@ -3,4 +3,4 @@
 #include "GitTest.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GitTest, "GitTest" );
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GitTest, "GitTest" ); //
